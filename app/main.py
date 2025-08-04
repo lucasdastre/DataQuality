7f8db75fe8c9c8561179eca0fec758397f76a7fc
Lucas <lucasdastre10@gmail.com>
2025-08-04 09:53:48 -0300

    feat/testando codigo

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, field_validator, ValidationError
 
+
 class Operacao(BaseModel):
     x: int
     y: int
@@ -7,13 +8,20 @@ class Operacao(BaseModel):
     @field_validator("x", "y")
     def valores_devem_ser_positivos(cls, valor, info):
         if valor < 0:
-            raise ValueError(f"O campo '{info.field_name}' não pode ser negativo.")
+            raise ValueError(f"O campo '{info.field_name}' não negativo.")
         return valor
+
 
 def soma(operacao: Operacao):
     return operacao.x + operacao.y
 
+
+def funcao_ola():
+    return "Olá, mundo!"
+
+
 if __name__ == "__main__":
+    funcao_ola()
 
     # ✅ Teste com valores válidos
     try:
