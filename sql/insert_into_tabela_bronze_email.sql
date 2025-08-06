fadf34bf2f1e26f5a5930b21202eb580850fb85a
Lucas <lucasdastre10@gmail.com>
2025-08-06 19:42:45 -0300

    doc/segunda mudanca sql

--- sql/insert_into_tabela_bronze_email.sql
+++ sql/insert_into_tabela_bronze_email.sql
@@ -1,4 +1,4 @@
-INSERT INTO produtos_bronze (nome, quantidade, preco, categoria, email) VALUES
+INSERT INTO produtos_bronze_email (nome, quantidade, preco, categoria, email) VALUES
 ('Produto A', 100, 10.0, 'eletronicos','produtoA@example.com' ),
 ('Produto B', 150, 20.0, 'mobilia', 'produtoA@example.com'),
 ('Produto C', 200, 15.0, 'informatica', 'produtoA@example.com'),
